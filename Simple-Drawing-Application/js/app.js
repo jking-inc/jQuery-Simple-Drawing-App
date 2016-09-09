2b412383b82be5b400d0b13e8a5a50abb7dc37e4
Joshua King <joshuaking85@gmail.com>
2016-09-09 15:28:21 +0000

    initial commit

---
+++ Simple-Drawing-Application/js/app.js
@@ -0,0 +1,19 @@
+// Problem : User interaction causes no changes to applications
+// Solution : When user interacts, cause changes appropriately
+
+// WHen clicking on control list items
+  // De-select sibling elements
+  // select clicked element
+
+// When add color is pressed
+  // Show color select or hide it
+
+// When color sliders change
+  // Update the new color span
+
+// When add color is pressed
+  // Append the color to the controls ul
+  // Select the new color
+
+// On mouse events on the canvas
+  // Draw Lines
