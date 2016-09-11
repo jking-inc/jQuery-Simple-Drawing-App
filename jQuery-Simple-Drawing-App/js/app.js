2f923d7b6b78a36a34fce76c5325944edcd54592
Joshua King <joshuaking85@gmail.com>
2016-09-11 15:00:55 +0000

    App is now able to draw on the canvas

--- jQuery-Simple-Drawing-App/js/app.js
+++ jQuery-Simple-Drawing-App/js/app.js
@@ -1,6 +1,9 @@
 // Problem : User interaction causes no changes to applications
 // Solution : When user interacts, cause changes appropriately
 var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
 
 // WHen clicking on control list items
 $(".controls").on("click", "li", function(){
@@ -40,4 +43,24 @@ $("#addNewColor").click(function(){
   $newColor.click();              
 });
 // On mouse events on the canvas
+$canvas.mousedown(function(e){
+  lastEvent = e;
+  mouseDown = true;
+}).mousemove(function(e){
   // Draw Lines
+  if(mouseDown) {
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.stroke();
+    lastEvent = e;
+  }
+}).mouseup(function(){
+  mouseDown = false;
+}).mouseleave(function(){
+  $canvas.mouseup();
+});
+  
+
+
